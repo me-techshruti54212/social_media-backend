ba5610b9456466f8972779bc77770803a877097c
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-09-23 15:32:18 +0530

    deployed frontend url added to connect to socket io

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require("dotenv").config()
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: 'http://localhost:3000', 
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000', 
     methods: ['GET', 'POST'],
     credentials: true
   }
@@ -27,7 +27,7 @@ const bodyParser = require("body-parser");
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin:process.env.FRONTEND_URL || 'http://localhost:3000',
   methods: ['GET', 'POST'],
   credentials: true
 }));
