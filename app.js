23c2c38f15413beffc95d49275854c6dd309c498
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-09-23 22:11:39 +0530

    made changes to run on localhost

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ require("dotenv").config()
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin:'*' ||  'https://social-media-frontend-flame.vercel.app',
+    // origin:'*' ||  'https://social-media-frontend-flame.vercel.app',
+    origin:'http://localhost:3000',
     methods: ['GET', 'POST'],
     credentials: true,
     
@@ -31,7 +32,8 @@ const bodyParser = require("body-parser");
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:3000', 'https://social-media-frontend-flame.vercel.app'],
+  // origin: [process.env.FRONTEND_URL || 'http://localhost:3000', 'https://social-media-frontend-flame.vercel.app'],
+  origin:'http://localhost:3000',
   methods: ['GET', 'POST'],
   credentials: true,
   allowedHeaders: ["Content-Type"],
